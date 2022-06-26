e9dcc8c9d0d3be2c1a6c9afc5f3356dc4a6c40bd
Chitranjan Babu <chitranjan.pc8@gmail.com>
2022-06-26 11:26:15 +0530

    terraform - default variable

--- hello.tf
+++ hello.tf
@@ -1,7 +1,13 @@
 output "hello" {
-  value = "Hello World ${var.name}"
+  value = "Hello World ${var.name} | age ${var.age}"
 }
 
 variable "name" {
+  type    = string
+  default = "Guest"
+}
 
+variable "age" {
+  type    = number
+  default = 20
 }

40aa7ae6652d3fe8dea47aed72681f234c98015f
Chitranjan Babu <chitranjan.pc8@gmail.com>
2022-06-26 11:53:45 +0530

    terraform - function join

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ output "hello" {
   #terraform plan --var "age=20.6"
   value = "Hello World ${upper(var.name)} | age ${ceil(var.age)}"
 }
+
+output "display-users" {
+  value = "Users : ${join(", ", var.users)}"
+}
--- variable.tf
+++ variable.tf
@@ -8,3 +8,8 @@ variable "age" {
   type    = number
   default = 20
 }
+
+variable "users" {
+  type    = list(any)
+  default = ["Ram", "Mohan", "Shyam"]
+}

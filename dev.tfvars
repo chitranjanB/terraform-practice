51a3abe1c7883937ad78873cb89ab5d94c264160
Chitranjan Babu <chitranjan.pc8@gmail.com>
2023-04-13 13:17:31 +0530

    Terraform - AWS - initial commit

--- dev.tfvars
+++
@@ -1,4 +0,0 @@
-#terraform plan -var-file=dev.tfvars
-name  = "UserDev"
-age   = 29
-users = ["UserDev"]
--- main.tf
+++
@@ -1,8 +0,0 @@
-output "hello" {
-  #terraform plan --var "age=20.6"
-  value = "Hello World ${upper(var.name)} | age ${ceil(var.age)}"
-}
-
-output "display-users" {
-  value = "Users : ${join(", ", var.users)}"
-}
